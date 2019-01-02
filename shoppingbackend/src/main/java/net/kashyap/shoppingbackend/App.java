13270f8151d7558f8396e1a158d0839e8ad5848f
Kashyap <swetangkashyap@gmail.com>
2019-01-03 00:52:50 +0530

    My project setup completed successfully.

---
+++ shoppingbackend/src/main/java/net/kashyap/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kashyap.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
